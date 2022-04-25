86f2e573ee2fea69d2572ee5fe6d2b73ee62e306
tomerOko <tomeroko2@gmail.com>
2022-04-25 17:02:44 +0300

    todos

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log("halloww")
+/**
+ * 'index.ts' resposabilities:
+ *      a. eventually load the server
+ *      b. helth check
+ *      c. log about beginning os service initialization proccess
+ *      d. load environment variables (port, dev/prod/test, ) 
+ *      e. load dependency container? (maybe just set the NODE_ENV value
+ *         and let the dependency container module take care about loading
+ *         the right dependencies)
+ *      f. create and check connections:
+ *          1. persistance DB
+ *          2. cach DB
+ *          3. third
+ */
+
